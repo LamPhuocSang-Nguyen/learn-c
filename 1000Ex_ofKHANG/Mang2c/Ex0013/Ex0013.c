0e15a7633a5f1e426de725dcf69e6124ef16e399
unknown <LPSang@DESKTOP-8FL2I08>
2021-08-17 07:04:36 +0700

    Toi bai 125 mang2c

---
+++ 1000Ex_ofKHANG/Mang2c/Ex0013/Ex0013.c
@@ -0,0 +1,91 @@
+//&&0125
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 100
+
+void InputElement(int* m, int* n, int a[][MAX]);
+void OutputElement(int m, int n, int a[][MAX]);
+void  Conversion(int m, int n, int a[][MAX]);
+void Swap(int* a, int* b);
+
+int main(void)
+{
+    int a[MAX][MAX];
+    int m;
+    int n;
+
+    InputElement(&m,&n,a);
+
+    OutputElement(m,n,a);
+
+    Conversion(m,n,a);
+
+    OutputElement(m,n,a);
+
+    return 0;
+}
+
+void InputElement(int* m, int* n, int a[][MAX])
+{
+    printf("Enter the number of row: ");
+    scanf("%d", m);
+
+    printf("Enter the number of column: ");
+    scanf("%d", n);
+
+    srand((time(NULL)));
+
+    for(int i = 0; i < *m; i++)
+    {
+        for(int j = 0; j < *n; j++)
+        {
+            a[i][j] = rand() % (100 + 100) - 100;
+        }
+    }
+}
+
+void OutputElement(int m, int n, int a[][MAX])
+{
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            printf("a[%d][%d] = ", i, j);
+            printf("%d",a[i][j]);
+            printf("\t");
+        }
+        printf("\n");
+    }
+
+}
+
+void Swap(int* a, int* b)
+{
+    int temp = *a;
+
+    *a = *b;
+
+    *b = temp;
+}
+
+void Conversion(int m, int n, int a[][MAX])
+{
+    int c;
+
+    printf("Enter column you want: ");
+
+    scanf("%d", &c);
+
+    for(int i = 0; i < m - 1; i++)
+    {
+        for(int j = i + 1; j < m; j++)
+        {
+            if(a[i][c] > a[j][c])
+            {
+                Swap(&a[i][c], &a[j][c]);
+            }
+        }
+    }
+}
